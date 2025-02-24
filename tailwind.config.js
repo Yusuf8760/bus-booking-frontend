edb7e493beeba88794d691a8e4ece72e3f4d6d44
Yusuf8760 <shiprayusuf876@gmail.com>
2025-02-24 09:24:32 +0000

    tlwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"], // Ensures Tailwind scans all React files
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
 
